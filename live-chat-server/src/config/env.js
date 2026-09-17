3bc2644e7e0d12d67e33878ba6efcf484d50a602
Hashir Ahmed <ahmedhashir346@gmail.com>
2026-09-18 02:18:36 +0500

    Add User Schema

--- live-chat-server/src/config/env.js
+++ live-chat-server/src/config/env.js
@@ -1,1 +1,10 @@
 MONGO_URI=mongodb//127.0.0.1:27017/liveChat
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const PORT = process.env.PORT || 5000;
+
+export const MONGO_URI = process.env.MONGO_URI;
+
+export const JWT_SECRET = process.env.JWT_SECRET;
